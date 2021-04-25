75b6f2e31fff1edbf8ff6e481b422cf0a65c7a70
niusmallnan <niusmallnan@gmail.com>
2021-04-25 10:16:46 +0800

    PANDARIA: allow none plugin to enable networkpolicy
    Signed-off-by: niusmallnan <niusmallnan@gmail.com>

--- pkg/api/store/cluster/cluster_store.go
+++ pkg/api/store/cluster/cluster_store.go
@@ -706,7 +706,7 @@ func validateNetworkFlag(data map[string]interface{}, create bool) error {
 			}
 			return fmt.Errorf("enableNetworkPolicy should be false for non-RKE clusters")
 		}
-		if plugin != "canal" && plugin != "multus-canal-macvlan" {
+		if plugin != "canal" && plugin != "multus-canal-macvlan" && plugin != "none" {
 			return fmt.Errorf("plugin %s should have enableNetworkPolicy %v", plugin, !value)
 		}
 	}
